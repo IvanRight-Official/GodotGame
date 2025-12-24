6809f966cdf17bad65a0fb1a80816d821bc2a18d
ivan <ivan_work@qq.com>
2025-12-24 13:26:29 +0800

    1、通过代码控制角色动画的播放

---
+++ Script/Envm/DirectionEnum.cs
@@ -0,0 +1,14 @@
+// 2025-12-24 12:09:37
+
+namespace FirstGodotGame.Script.Envm;
+
+/// <summary>
+/// 方向枚举
+/// </summary>
+public enum DirectionEnum
+{
+    Left,
+    Right,
+    Up,
+    Down
+}
